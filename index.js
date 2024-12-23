74a4131952d905c61da20a13f5c6d4107883dde9
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2024-12-23 19:34:19 +0200

    Added the search functionality using the API

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  // make api call and update the UI
+  let apiKey = "dfa0t3a70dbo782ca3e7d359f6458d45";
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiurl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
-  console.log(cityElement);
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Kigali");
