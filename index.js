ec47ca749aafb3d48406d6ae24b263c40963ca9a
Assumpta-tech <uwamariya.assumpta@yahoo.com>
2024-12-23 18:32:06 +0200

    Added id attribute to the form

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#cities");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  console.log(cityElement);
 }
 
 let searchFormElement = document.querySelector("#search-form");
